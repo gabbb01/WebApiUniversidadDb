7f2c317afcedff387859f8df9428a84ce4ff665b
Sheyly Turcios <orellanasheyly@gmail.com>
2026-03-29 21:53:04 -0600

    Implementacion de UniversidadDbContext por Sheyly Turcios

--- WebApiUniversidadDb/Infrastructure/Databases/UniversidadDbContext.cs
+++ WebApiUniversidadDb/Infrastructure/Databases/UniversidadDbContext.cs
@@ -1,8 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApiUniversidadDb.Entities;
 
 namespace WebApiUniversidadDb.Infrastructure.Databases
 {
-    public class UniversidadDbContet : DbContext
+    public class UniversidadDbContext : DbContext
     {
+        public UniversidadDbContext(DbContextOptions<UniversidadDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asignatura> Asignaturas { get; set; }
+        public DbSet<Aula> Aulas { get; set; }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+        public DbSet<Profesor> Profesores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
