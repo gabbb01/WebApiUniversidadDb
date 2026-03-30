a29559319b1f8a112c2233ad425cc1f93e5038ed
Ruth Samanta Chavarria <samantachavarria15@gmail>
2026-03-29 21:08:30 -0600

    Entities de Universidad completas

--- WebApiUniversidadDb/Entities/Profesor.cs
+++ WebApiUniversidadDb/Entities/Profesor.cs
@@ -1,6 +1,12 @@
-﻿namespace WebApiUniversidadDb.Entities
+﻿using System;
+
+namespace WebApiUniversidadDb.Entities
 {
     public class Profesor
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public bool Activo { get; set; }
+        public DateTime FechaCreacion { get; set; }
     }
-}
+}   
