d76fee3f381224b1d24a5b546edf7689aad902fe
gabbb01 <gabrielfunes2223@gmail.com>
2026-03-31 10:53:27 -0600

    app services

--- WebApiUniversidadDb/Features/Universidad/AppServices/AsignaturasAppService.cs
+++ WebApiUniversidadDb/Features/Universidad/AppServices/AsignaturasAppService.cs
@@ -1,6 +1,73 @@
-﻿namespace WebApiUniversidadDb.Features.Universidad.AppServices
+﻿using WebApiUniversidadDb.Commons.Models;
+using WebApiUniversidadDb.Entities;
+using WebApiUniversidadDb.Features.Universidad.DomainServices;
+using WebApiUniversidadDb.Features.Universidad.Interfaces;
+using WebApiUniversidadDb.Infrastructure.Interfaces;
+
+namespace WebApiUniversidadDb.Features.Universidad.AppServices
 {
-    public class AsignaturasAppService
+    public class AsignaturasAppService : IAsignaturasAppService
     {
+        private readonly IAsignaturasRepository asignaturasRepository;
+        private readonly AsignaturasDomainService asignaturasDomainService;
+        public AsignaturasAppService(IAsignaturasRepository asignaturasRepository, AsignaturasDomainService asignaturasDomainService)
+        {
+            this.asignaturasRepository = asignaturasRepository;
+            this.asignaturasDomainService = asignaturasDomainService;
+        }
+        public async Task<ApiResponse<Asignatura>> ActualizarAsignatura(Asignatura asignatura)
+        {
+            ApiResponse<Asignatura> apiResponseResult =
+                asignaturasDomainService.ActualizarAsignatura(asignatura);
+            try
+            {
+                if (apiResponseResult.Success)
+                {
+                    await asignaturasRepository.ActualizarAsignatura(asignatura);
+                }
+                return apiResponseResult;
+            }
+            catch (Exception ex)
+            {
+                apiResponseResult.Success = false;
+                apiResponseResult.Message = ex.Message;
+                return apiResponseResult;
+            }
+        }
+
+        public async Task<ApiResponse<Asignatura>> AgregarAsignatura(Asignatura asignatura)
+        {
+            ApiResponse<Asignatura> apiResponseResult =
+                 asignaturasDomainService.AgregarAsignatura(asignatura);
+            try
+            {
+                if (apiResponseResult.Success)
+                {
+                    await asignaturasRepository.AgregarAsignatura(asignatura);
+                }
+                return apiResponseResult;
+            }
+            catch (Exception ex)
+            {
+                apiResponseResult.Success = false;
+                apiResponseResult.Message = ex.Message;
+                return apiResponseResult;
+            }
+        }
+
+        public async Task InactivarAsignatura(int id)
+        {
+            await asignaturasRepository.InactivarAsignatura(id);
+        }
+
+        public async Task<List<Asignatura>> ObtenerAsignatura()
+        {
+            return await asignaturasRepository.ObtenerAsignaturas();
+        }
+
+        public async Task<Asignatura?> ObtenerAsignaturaId(int id)
+        {
+            return await asignaturasRepository.ObtenerAsignaturaId(id);
+        }
     }
 }
--- WebApiUniversidadDb/Features/Universidad/Interfaces/IAulasAppService.cs
+++ WebApiUniversidadDb/Features/Universidad/Interfaces/IAulasAppService.cs
@@ -1,6 +1,14 @@
-﻿namespace WebApiUniversidadDb.Features.Universidad.Interfaces
+﻿using WebApiUniversidadDb.Commons.Models;
+using WebApiUniversidadDb.Entities;
+
+namespace WebApiUniversidadDb.Features.Universidad.Interfaces
 {
     public interface IAulasAppService
     {
+        Task<List<Aula>> ObtenerAula();
+        Task<ApiResponse<Aula>> AgregarAula(Aula aula);
+        Task<ApiResponse<Aula>> ActualizarAula(Aula aula);
+        Task<Aula?> ObtenerAulaId(int id);
+        Task InactivarAula(int id);
     }
 }
